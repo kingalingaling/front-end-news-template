4897148d77bb7e94fe910b7f248dffe5d2dc2335
King Eshiebor <kingeshiebor@gmail.com>
2023-02-24 09:41:56 +0100

    Added Form validation

--- multi-step-form/scripts/script.js
+++ multi-step-form/scripts/script.js
@@ -1,59 +1,70 @@
+//DiV IDs
 const p_info = document.getElementById('step-1')
-const p_info_button = document.getElementById('submit-p-info')
 const select_plan = document.getElementById('step-2')
-const select_plan_button = document.getElementById('submit-plan') 
-const back_to_step_1 = document.getElementById('back-to-step-1')
 const add_ons = document.getElementById('step-3')
-const add_ons_button = document.getElementById('submit-add-ons')
-const back_to_step_2 = document.getElementById('back-to-step-2')
 const summary = document.getElementById('step-4')
-const finalize = document.getElementById('confirm')
-const back_to_step_3 = document.getElementById('back-to-step-3')
 const thanks = document.getElementById('step-5')
 
-p_info_button.onclick = function (){
-    p_info.classList.remove('show')
-    select_plan.classList.add('show')
+//Submit IDs
+const p_info_button = document.getElementById('submit-p-info')
+const select_plan_button = document.getElementById('submit-plan')
+const add_ons_button = document.getElementById('submit-add-ons')
+const finalize = document.getElementById('confirm')
+
+//Back IDs
+const back_to_step_1 = document.getElementById('back-to-step-1')
+const back_to_step_2 = document.getElementById('back-to-step-2')
+const back_to_step_3 = document.getElementById('back-to-step-3')
+
+p_info_button.addEventListener('click', function () {
+    validate(fieldName=['name', 'email', 'phone'], errorName=['nameError', 'emailError', 'phoneError'], current=p_info, next=select_plan)
+})
+
+function validate(fieldName, errorName, current, next) {
+    let valid = true
+    let errCount = 0
+    for(let i=0;i<fieldName.length;i++) {
+        const field = document.getElementById(fieldName[i])
+        const error = document.getElementById(errorName[i])
+        if(!field.value) {
+            error.classList.add('visible')
+            field.classList.add('invalid')
+            errCount++
+        } else {
+            error.classList.remove('visible')
+            field.classList.remove('invalid')            
+        }
+    }
+    console.log(`No. of empty fields - ${errCount}`)
+    if(errCount < 1){
+        current.classList.remove('show')
+        next.classList.add('show')
+    }
+    return valid
 }
 
-select_plan_button.onclick = function() {
-    select_plan.classList.remove('show')
-    add_ons.classList.add('show')
+let clicks_forward = [select_plan_button, add_ons_button, finalize]
+let clicks_backward = [back_to_step_1, back_to_step_2, back_to_step_3]
+let pages = [select_plan, add_ons, summary, thanks]
+
+for (let click of clicks_forward) {
+    let num = clicks_forward.indexOf(click)
+    click.onclick = function() {
+        pages[num].classList.remove('show')
+        pages[num+1].classList.add('show')
+    }
 }
 
-back_to_step_1.onclick = function() {
-    select_plan.classList.remove('show')
-    p_info.classList.add('show')
+for (let click of clicks_backward) {
+    let num = clicks_backward.indexOf(click)
+    click.onclick = function() {
+        if(num==0) {
+            pages[num].classList.remove('show')
+            p_info.classList.add('show')
+
+        } else {
+            pages[num].classList.remove('show')
+            pages[num-1].classList.add('show')
+        }
+    }
 }
-
-add_ons_button.onclick = function() {
-    add_ons.classList.remove('show')
-    summary.classList.add('show')
-}
-
-back_to_step_2.onclick = function() {
-    add_ons.classList.remove('show')
-    select_plan.classList.add('show')
-}
-
-finalize.onclick = function() {
-    summary.classList.remove('show')
-    thanks.classList.add('show')
-    document.getElementsByClassName('progress-mobile').display = 'none'
-}
-
-back_to_step_3.onclick = function() {
-    summary.classList.remove('show')
-    add_ons.classList.add('show')
-}
-
-/* if (p_info.classList.contains('show')) {
-    console.log('yeah')
-} else {
-    console.log('Nope')
-} */
-
-/* document.getElementById('submit-p-info').submit = function() {
-    document.getElementById('step-1').classList.remove('show')
-    document.getElementById('step-2').classList.add('show')
-} */
